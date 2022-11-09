959344088465ae93d3d7c24b072ee21463041275
Otherwa <67428572+Atharv2002@users.noreply.github.com>
2022-11-09 18:40:31 +0530

    /minor/

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: "8mjyru",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-  },
-  projectId: "8mjyru"
+  }
 })
