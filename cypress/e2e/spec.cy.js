2990b5b7d9c043d6d89aeacab8afea1efb281deb
Otherwa <67428572+Atharv2002@users.noreply.github.com>
2022-11-09 19:57:19 +0530

    /testing setup/

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,8 @@
+describe('gets the homepage', () => {
+  it('passes', () => {
+    cy.visit('https://levaccine.herokuapp.com/').then((contentWindow) => {
+      // contentWindow is the remote page's window object
+      console.log(contentWindow);
+    })
+  })
+})
