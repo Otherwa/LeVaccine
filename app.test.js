39d37d3dd812b5391f636b57ef7c3a52f13b7df6
Otherwa <67428572+Atharv2002@users.noreply.github.com>
2022-11-10 16:21:50 +0530

    /update/

--- app.test.js
+++ app.test.js
@@ -1,10 +1,28 @@
 const request = require('supertest');
 const app = require('./app')
 
-describe('GET /', function () {
-    it('get homepage', async function () {
+describe('GET All Pages', function () {
+    it('get page1', async function () {
         const response = await request(app)
             .get('/')
+        expect(response.status).toEqual(200);
+    });
+
+    it('get page2', async function () {
+        const response = await request(app)
+            .get('/education')
+        expect(response.status).toEqual(200);
+    });
+
+    it('get page3', async function () {
+        const response = await request(app)
+            .get('/contact')
+        expect(response.status).toEqual(200);
+    });
+
+    it('get page4', async function () {
+        const response = await request(app)
+            .get('/about')
         expect(response.status).toEqual(200);
     });
 });
