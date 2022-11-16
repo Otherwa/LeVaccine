de3d81d812ee461d458c15c32f58e13062165357
Atharv Desai <atharvdesai2002@gmail.com>
2022-11-16 03:44:54 +0000

    prop changes

--- config/connect.js
+++ config/connect.js
@@ -5,7 +5,9 @@ async function connect() {
     mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
     const mongo = mongoose.connection;
     mongo.on("error", () => { console.log("error in Conection service"); })
-    mongo.once('open', () => { console.log("Connected service"); })
+    mongo.once('open', () => {
+        console.log("Mongoose Connection Connected 200");
+    })
 }
 
 async function dis() {
