100dc6857bc34d7bc8c416b37d03f4f6c8f6edcb
Atharv Desai <atharvdesai2002@gmail.com>
2023-01-31 22:47:49 +0530

    Otherwa (#168)
    * /minor fix/
    
    * /help Api/
    
    * /profile validations, book appointmens valilation feature req/
    
    * /**required changes**/
    
    * /set appo limit fix/
    
    * /call-back issue/
    
    * /minor fix/
    
    * /sort changes/
    
    * /major changes/
    
    * /minor changes/
    
    * /minor/
    
    * /producer started/
    
    * /minor/
    
    * /minor/
    
    * minor changes
    
    ---------
    
    Co-authored-by: Otherwa <67428572+Atharv2002@users.noreply.github.com>

--- public/js/login.js
+++ public/js/login.js
@@ -50,20 +50,19 @@ $('#password').on('input', () => {
 
 function onSuccess(googleUser) {
     console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-
+    fetch('https://drug-lord.onrender.com/account/signup', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: {}
+    })
+        .then(response => response.json())
+        .then(response => console.log(JSON.stringify(response)))
 
 }
+
 function onFailure(error) {
     console.log(error);
-}
-function renderButton() {
-    gapi.signin2.render('my-signin2', {
-        'scope': 'profile email',
-        'width': 240,
-        'height': 50,
-        'longtitle': true,
-        'theme': 'dark',
-        'onsuccess': onSuccess,
-        'onfailure': onFailure
-    });
 }
